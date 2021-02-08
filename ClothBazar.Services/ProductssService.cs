eeffb99d6c52ee137be9a237b3ec17cac064ba40
Basharat Iqbal <basharat_iqbal786@hotmail.com>
2021-02-08 10:12:50 +0500

    CRUD Operations in One Page using Ajax

---
+++ ClothBazar.Services/ProductssService.cs
@@ -0,0 +1,45 @@
+﻿using ClothBazar.Database;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class ProductsService
+    {
+        CBContext db = new CBContext();
+        public Product GetProduct(int Id)
+        {
+            return db.Products.Find(Id);
+
+        }
+        public List<Product> GetProducts()
+        {
+            return db.Products.ToList(); 
+
+        }
+        public void SaveProduct(Product product)
+        {
+                db.Products.Add(product);
+                db.SaveChanges();
+            
+        }
+        public void UpdateProduct(Product product)
+        {
+            db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+
+        }
+        public void DeleteProduct(int Id)
+        {
+            var product = db.Products.Find(Id);
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        public object PName { get; private set; }
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+            if(search != null)
+            {
+                products = products.Where(p =>p.Name  != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+            return PartialView(products);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product )
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+        
+    }
+}
