f86d90a9e3120557185a042421a6a93d092dff0c
Basharat Iqbal <basharat_iqbal786@hotmail.com>
2021-02-18 10:43:57 +0500

    Uploading Pictures

---
+++ ClothBazar.Web/ViewModels/HomeViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothBazar.Web.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Category> Categories { get; set; }
+        public List<Product> products { get; set; }
+
+    }
+}
